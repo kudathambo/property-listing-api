ee6645081a27e8cd8034a163cf2901641d34f1e9
Kuda <kudat@actionstarter.co.uk>
2023-02-01 10:10:27 +0200

    Secure property modification routes

--- routes/api.php
+++ routes/api.php
@@ -26,13 +26,16 @@ Route::post('/login',[AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/brokers', [BrokersController::class, 'index']);
 Route::get('/brokers/{broker}', [BrokersController::class, 'show']);
-
-Route::apiResource('/properties', PropertiesController::class);
-
+Route::get('/properties', [PropertiesController::class, 'index']);
+Route::get('/properties/{property}', [PropertiesController::class, 'show']);
 
 Route::group(['middleware' => ['auth:sanctum']], function (){
 
     Route::apiResource('/brokers', BrokersController::class)->only([
+        'store', 'update', 'destroy'
+    ]);
+
+    Route::apiResource('/properties', PropertiesController::class)->only([
         'store', 'update', 'destroy'
     ]);
     Route::post('/logout', [AuthController::class, 'logout']);
